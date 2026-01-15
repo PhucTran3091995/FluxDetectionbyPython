05a0b1d607a5cca27bd327d00865ecabeb0e18f3
PhucTran3091995 <phuctq95@gmail.com>
2026-01-15 10:33:22 +0700

    Remove server-related files for pure client repo

--- server_deploy/install_dependencies.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title Install Requirements
-echo Installing necessary libraries...
-pip install opencv-python numpy mysql-connector-python pyyaml onnxruntime
-echo.
-echo Installation complete! You can now run run_server.bat
-pause
--- server_deploy/run_server.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-title FluxApp Server Service
-echo Starting Server...
-python server_main.py
-pause
